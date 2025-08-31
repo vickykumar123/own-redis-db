e281414ee6e93700f4fea4118132fd13280fec73
vicky <vickykumarsunilja@gmail.com>
2025-08-31 17:34:14 +0530

    Fixed the issue

--- app/main.ts
+++ app/main.ts
@@ -8,11 +8,7 @@ const server: net.Server = net.createServer((socket: net.Socket) => {
   // Handle connection
   socket.on("data", (data: Buffer) => {
     const command = data.toString().trim();
-    const input = command.split("\n");
-    for (let i = 0; i < input.length; i++) {
-      console.log(input[i]);
-      socket.write("+PONG\r\n");
-    }
+    socket.write("+PONG\r\n");
   });
   socket.write("+PONG\r\n");
   socket.end();
