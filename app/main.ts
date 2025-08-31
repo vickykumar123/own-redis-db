b65ca2ad53100ac14f01ca35906b61e613e4703a
vicky <vickykumarsunilja@gmail.com>
2025-08-31 17:32:38 +0530

    Added the multiple response

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((socket: net.Socket) => {
   // Handle connection
+  socket.on("data", (data: Buffer) => {
+    const command = data.toString().trim();
+    const input = command.split("\n");
+    for (let i = 0; i < input.length; i++) {
+      console.log(input[i]);
+      socket.write("+PONG\r\n");
+    }
+  });
   socket.write("+PONG\r\n");
   socket.end();
 });
