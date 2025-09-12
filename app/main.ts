e04f3e6553f0b3a33acb1bf44558dacdffd777b7
vicky <vickykumarsunilja@gmail.com>
2025-09-12 08:20:51 +0530

    Added the support to the custom ports

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,12 @@ console.log("Logs from your program will appear here!");
 
 const redisCommands = new RedisCommands();
 
+let port = 6379;
+const portIndex = process.argv.indexOf("--port");
+if (portIndex !== -1 && process.argv.length > portIndex + 1) {
+  port = parseInt(process.argv[portIndex + 1]);
+}
+
 const server: net.Server = net.createServer((socket: net.Socket) => {
   // Handle connection
   socket.on("data", async (data: Buffer) => {
@@ -35,4 +41,4 @@ const server: net.Server = net.createServer((socket: net.Socket) => {
   });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(port, "127.0.0.1");
